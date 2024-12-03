cbafe0ecacde0dc8dd65fb5b4bd56123d8cd3053
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-12-03 15:01:48 -0300

    fix/implementation, correções da logica de atualização de livros e autores

--- src/api/book.api.ts
+++ src/api/book.api.ts
@@ -2,22 +2,42 @@ import axios from "axios";
 import { Book } from "../interfaces";
 import { reactBackendUrl } from "../env/envoriment";
 
-
 export const getAllBooks = async (): Promise<Book[]> => {
   const response = await axios.get(`${reactBackendUrl}/books`);
-  return response.data;
+  return response.data.map((book: any) => {
+    return {
+      ...book,
+      authors: {
+        id: book.authors.id || null,
+        name: book.authors.name || null,
+        birthDate: book.authors.birthDate || null,
+      },
+    };
+  });
 };
 
 export const createBook = async (book: Partial<Book>): Promise<Book> => {
-  const response = await axios.post(`${reactBackendUrl}/books`, book);
+  const body = {
+    ...book,
+    id: Number(book.authors?.id) ?? null,
+  };
+  const response = await axios.post(`${reactBackendUrl}/books`, body);
+
   return response.data;
 };
 
-export const updateBook = async (id: number, book: Partial<Book>): Promise<Book> => {
-  const response = await axios.put(`${reactBackendUrl}/books/${id}`, book);
-  if(!response.data) throw new Error("Book not found");
+export const updateBook = async (
+  id: number,
+  book: Partial<Book>,
+): Promise<Book> => {
+  const body = {
+    ...book,
+    id: Number(book.authors?.id) ?? null,
+  };
+  const response = await axios.patch(`${reactBackendUrl}/books/${id}`, body);
+  if (!response.data) throw new Error("Book not found");
   return response.data;
-}
+};
 
 export const deleteBook = async (id: number): Promise<void> => {
   await axios.delete(`${reactBackendUrl}/books/${id}`);
